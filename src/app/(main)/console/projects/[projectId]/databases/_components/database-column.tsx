009c51c1548d8adcf58a94a630c81eb9849eeba5
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-04-20 13:43:39 -0400

    feat(frontend): added delete and edit actions to database table

--- src/app/(main)/console/projects/[projectId]/databases/_components/database-column.tsx
+++ src/app/(main)/console/projects/[projectId]/databases/_components/database-column.tsx
@@ -1,11 +1,36 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
+import CreateDatabaseForm from '@/components/forms/create-database-form'
+import CustomModal from '@/components/modals/custom-modal'
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { Button, buttonVariants } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { useToast } from '@/components/ui/use-toast'
+import { deleteDatabase } from '@/controllers/database-controller'
 import { ExtendedDatabase } from '@/lib/types'
+import { useModal } from '@/providers/modal-provider'
 import { ColumnDef } from '@tanstack/react-table'
 import { format } from 'date-fns'
-import { ArrowUpDown } from 'lucide-react'
+import { ArrowUpDown, Edit, MoreHorizontal, Trash2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 export const DatabaseColumns: ColumnDef<ExtendedDatabase>[] = [
 	{
@@ -130,24 +155,99 @@ export const DatabaseColumns: ColumnDef<ExtendedDatabase>[] = [
 		id: 'actions',
 		cell: ({ row }) => {
 			const rowData = row.original
-			return <div className='flex items-center gap-x-1'></div>
+			return (
+				<div className='flex items-center gap-x-1'>
+					<CellActions rowData={rowData} />
+				</div>
+			)
 		},
 	},
 ]
 
-/* function CellActions({ rowData }: CellActionsProps) {
+interface CellActionsProps {
+	rowData: ExtendedDatabase
+}
+
+function CellActions({ rowData }: CellActionsProps) {
+	const { setOpen } = useModal()
+	const { toast } = useToast()
+	const router = useRouter()
+	const handleDelete = async () => {
+		const res = await deleteDatabase(rowData.projectId, rowData.id)
+		if (res.success) {
+			toast({
+				title: 'Database deleted',
+				variant: 'success',
+			})
+		} else {
+			toast({
+				title: 'Database could not be deleted',
+				description: res.message,
+				variant: 'destructive',
+			})
+		}
+		router.refresh()
+	}
+
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant='ghost' className='h-8 w-8 p-0'>
-					<span className='sr-only'>Open menu</span>
-					<MoreHorizontal className='h-4 w-4' />
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align='end'>
-				<DropdownMenuLabel>Actions</DropdownMenuLabel>
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<AlertDialog>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button variant='ghost' className='h-8 w-8 p-0'>
+						<span className='sr-only'>Open menu</span>
+						<MoreHorizontal className='h-4 w-4' />
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent align='end'>
+					<DropdownMenuLabel>Actions</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem
+						className='flex items-center gap-x-2'
+						onClick={() => {
+							setOpen(
+								<CustomModal>
+									<CreateDatabaseForm
+										projectId={rowData.projectId}
+										mode='edit'
+										database={rowData}
+									/>
+								</CustomModal>
+							)
+						}}
+					>
+						<Edit size={18} />
+						Edit
+					</DropdownMenuItem>
+					<AlertDialogTrigger asChild>
+						<DropdownMenuItem className='flex items-center gap-x-2 text-destructive'>
+							<Trash2 size={18} />
+							Delete
+						</DropdownMenuItem>
+					</AlertDialogTrigger>
+				</DropdownMenuContent>
+			</DropdownMenu>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Delete Database</AlertDialogTitle>
+					<AlertDialogDescription>
+						Are you sure you want to delete the database? This
+						action cannot be undone and all the data will be
+						lost
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+					<AlertDialogAction
+						className={buttonVariants({
+							variant: 'destructive',
+						})}
+						onClick={handleDelete}
+					>
+						Delete
+					</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
 	)
 }
- */
